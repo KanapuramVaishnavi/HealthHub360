2e7762ef00a7bc73d814d7efa539b9f6a404b093
KanapuramVaishnavi <kanapuramvaishnavi96401@gmail.com>
2025-11-11 13:22:04 +0530

    tenant model

--- Models/tenant.go
+++ Models/tenant.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Tenant struct {
-	TenantID      string    `json:"tenantID" bson:"tenantID"`
-	TenantName    string    `json:"tenantName" bson:"tenantName"`
-	TenantMail    string    `json:"tenantMail" bson:"tenantMail"`
-	TenantPhoneNo string    `json:"tenantPhoneNo" bson:"tenantPhoneNo"`
-	Password      string    `json:"password,omitempty" bson:"password"`
-	Token         string    `json:"token,omitempty" bson:"token"`
-	CreatedAt     time.Time `json:"createdAt" bson:"createdAt"`
-	UpdatedAt     time.Time `json:"updatedAt" bson:"updatedAt"`
+	TenantID  string    `json:"tenantID" bson:"tenantID"`
+	Name      string    `json:"name" bson:"name"`
+	Mail      string    `json:"mail" bson:"mail"`
+	PhoneNo   string    `json:"phoneNo" bson:"phoneNo"`
+	Password  string    `json:"password,omitempty" bson:"password"`
+	Token     string    `json:"token,omitempty" bson:"token"`
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
 }
