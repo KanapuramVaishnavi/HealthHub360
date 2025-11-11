206a4dcea236eeb7e57f4c38900015e8e097d404
adi041518 <kadambalaaditya@gmail.com>
2025-11-11 14:51:24 +0530

    ADITYA COMMIT

--- Middleware/jwtmiddleware.go
+++ Middleware/jwtmiddleware.go
@@ -34,3 +34,16 @@ func JWTAuth() gin.HandlerFunc {
 		c.Next()
 	}
 }
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // or specific domain
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
