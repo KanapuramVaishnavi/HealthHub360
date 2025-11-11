82828ea40c1b9d933161344b116d768a176d4708
adi041518 <kadambalaaditya@gmail.com>
2025-11-11 12:48:48 +0530

    Auth ADDED

--- Authentication/token.go
+++ Authentication/token.go
@@ -1,1 +1,40 @@
 package authentication
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("123456789")
+
+type JWTClaim struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	TenantID string `json:"tenant_id"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(id, name, email, tenantID string) (string, error) {
+	expMinutesStr := os.Getenv("JWT_EXPIRATION_TIME_IN_MINUTES")
+	expMinutes, err := strconv.Atoi(expMinutesStr)
+	if err != nil || expMinutes <= 0 {
+		expMinutes = 60
+	}
+
+	expHours := time.Duration(expMinutes) * time.Minute
+	claims := &JWTClaim{
+		ID:       id,
+		Name:     name,
+		Email:    email,
+		TenantID: tenantID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expHours)),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Error In Connecting with Env", err.Error())
+		return
+	}
 	router := gin.Default()
 	router.GET("/main", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
